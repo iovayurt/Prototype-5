98fbae63cf76c31b715445a12ae3c989c02fec51
Ilyas Ovayurt <iovayurt@gmail.com>
2022-05-13 12:52:23 +0300

    particles

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -13,6 +13,7 @@ public class Target : MonoBehaviour
     private float ySpawnPos = -3;
 
     public int pointValue;
+    public ParticleSystem explosionParticle;
 
     // Start is called before the first frame update
     void Start()
@@ -34,6 +35,7 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
         gameManager.UpdateScore(pointValue);
     }
 
