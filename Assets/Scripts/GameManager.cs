a7919a9293c5765559c241411acce2eec5cf93ac
Ilyas Ovayurt <iovayurt@gmail.com>
2022-05-13 16:14:05 +0300

    game over

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,7 @@ public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
     private int score;
     private float spawnRate = 1.0f;
 
@@ -15,6 +16,8 @@ public class GameManager : MonoBehaviour
     {
         StartCoroutine(SpawnTarget());
         UpdateScore(0);
+
+        gameOverText.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
